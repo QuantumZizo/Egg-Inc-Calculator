01fb8a62f22c29ef0eb4a4b5d632ff7ccc788821
QuantumZizo <zayn.elborno@13bananas.com>
2022-03-11 14:45:05 -0700

    Actual Functionality

---
+++ src/main/kotlin/com/quantumzizo/eggIncCalculator/Calculate.kt
@@ -0,0 +1,17 @@
+package com.quantumzizo.eggIncCalculator
+
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.pow
+class Calculate {}
+
+fun soulEggCalculate(prestEarnings:Double, eventBoost:Double): Double {
+    var z:Double = prestEarnings * eventBoost * (max(0.0,(10.0.pow(-6) * min(prestEarnings, 10.0.pow(12)))).pow(0.15) - (10.0.pow(-6).pow(0.15))
+            + max(0.0,(10.0.pow(-6) * min(prestEarnings, 10.0.pow(21)))).pow(0.16) - (10.0.pow(6).pow(0.16))
+            + max(0.0,(10.0.pow(-6) * min(prestEarnings, 10.0.pow(30)))).pow(0.17) - (10.0.pow(15).pow(0.17))
+            + max(0.0,(10.0.pow(-6) * min(prestEarnings, 10.0.pow(39)))).pow(0.18) - (10.0.pow(24).pow(0.18))
+            + max(0.0,(10.0.pow(-6) * min(prestEarnings, 10.0.pow(48)))).pow(0.19) - (10.0.pow(33).pow(0.19))
+            + max(0.0,(10.0.pow(-6) * min(prestEarnings, 10.0.pow(60)))).pow(0.20) - (10.0.pow(42).pow(0.20))
+            + max(0.0,(10.0.pow(-6) * prestEarnings)).pow(0.21) - (10.0.pow(54).pow(0.21)))
+    return z
+}
--- src/main/kotlin/com/quantumzizo/eggIncCalculator/Main.kt
+++ src/main/kotlin/com/quantumzizo/eggIncCalculator/Main.kt
@@ -1,2 +1,23 @@
 package com.quantumzizo.eggIncCalculator
 
+import kotlin.system.exitProcess
+
+class Main
+
+const val state = true
+var prestEarnings:Double = 0.0
+var eventBoost:Double = 0.0
+var soulEggReward:Double = 0.0
+
+fun main() {
+    while(state) {
+        try {
+            println("Enter your prestige earnings ingame (It can be checked in the stats menu")
+            prestEarnings = readln().toDouble()
+            println("Enter the event boost for soul eggs (If there is none, enter 1)")
+            eventBoost = readln().toDouble()
+            soulEggReward = soulEggCalculate(prestEarnings, eventBoost)
+            print(soulEggReward)
+        } catch(e: java.lang.NumberFormatException) {}
+    }
+}
