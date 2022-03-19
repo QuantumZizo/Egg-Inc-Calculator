eb6d9829bd9264b7b907b4803b2e4d9fafe73420
QuantumZizo <zayn.elborno@13bananas.com>
2022-03-19 13:01:24 -0600

    Disabled the soul egg multiplier input
    I did this because the soul egg multiplier does not work as i thought it would so i disabled it. I will reenable this feature once I figure out how it works

--- src/main/kotlin/com/quantumzizo/eggIncCalculator/Main.kt
+++ src/main/kotlin/com/quantumzizo/eggIncCalculator/Main.kt
@@ -20,8 +20,8 @@ fun main() {
             if(onOrOff == 'y' || onOrOff == 'Y') {
                 println("Enter the prestige earnings you would think you'd need to get an amount of soul eggs")
                 prestEarnings = readln().toDouble()
-                println("Enter the soul egg multiplier given by a boost in game (If you do not have a boost active, enter 1)")
-                prestBonus = readln().toDouble()
+                // println("Enter the soul egg multiplier given by a boost in game (If you do not have a boost active, enter 1)")
+                prestBonus = 0.0
                 println("Enter the event boost for soul eggs (If there is none, enter 1)")
                 eventBoost = readln().toDouble()
                 soulEggReward = soulEggCalculate(prestEarnings, prestBonus, eventBoost)
