49ab7d62fe61bf531486caa387402c7a21147d1b
QuantumZizo <zayn.elborno@13bananas.com>
2022-03-19 13:34:36 -0600

    Completely Removed the Prestige Bonus argument and added feature
    Since I realized that it would not be important, I completely removed the Prestige Bonus factor from the formula. I also added an addition to the formula that I can't explain here

--- src/main/kotlin/com/quantumzizo/eggIncCalculator/Main.kt
+++ src/main/kotlin/com/quantumzizo/eggIncCalculator/Main.kt
@@ -8,7 +8,6 @@ class Main
 const val state = true
 var onOrOff = 'n'
 var prestEarnings:Double = 0.0
-var prestBonus:Double = 0.0
 var eventBoost:Double = 0.0
 var soulEggReward:Double = 0.0
 
@@ -20,11 +19,9 @@ fun main() {
             if(onOrOff == 'y' || onOrOff == 'Y') {
                 println("Enter the prestige earnings you would think you'd need to get an amount of soul eggs")
                 prestEarnings = readln().toDouble()
-                // println("Enter the soul egg multiplier given by a boost in game (If you do not have a boost active, enter 1)")
-                prestBonus = 0.0
                 println("Enter the event boost for soul eggs (If there is none, enter 1)")
                 eventBoost = readln().toDouble()
-                soulEggReward = soulEggCalculate(prestEarnings, prestBonus, eventBoost)
+                soulEggReward = soulEggCalculate(prestEarnings, eventBoost)
                 println("You would receive ${soulEggReward.roundToLong()} soul eggs from prestige")
             } else if(onOrOff == 'n' || onOrOff == 'N') {
                 exitProcess(0)
--- src/main/kotlin/com/quantumzizo/eggIncCalculator/SoulEggCalculate.kt
+++ src/main/kotlin/com/quantumzizo/eggIncCalculator/SoulEggCalculate.kt
@@ -4,8 +4,9 @@ import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.pow
 
-fun soulEggCalculate(prestEarnings: Double, prestBonus: Double, eventBoost: Double): Double {
-    return prestBonus * eventBoost * (
+fun soulEggCalculate(prestEarnings: Double, eventBoost: Double): Double {
+    var soulEggReward:Double = 0.0
+    soulEggReward =  eventBoost * (
                       max(0.0, (10.0.pow(-6) * min(prestEarnings, 10.0.pow(12))).pow(0.15) - 10.0.pow(-6).pow(0.15))
                     + max(0.0, (10.0.pow(-6) * min(prestEarnings, 10.0.pow(21))).pow(0.16) - 10.0.pow(6).pow(0.16))
                     + max(0.0, (10.0.pow(-6) * min(prestEarnings, 10.0.pow(30))).pow(0.17) - 10.0.pow(15).pow(0.17))
@@ -14,4 +15,8 @@ fun soulEggCalculate(prestEarnings: Double, prestBonus: Double, eventBoost: Doub
                     + max(0.0, (10.0.pow(-6) * min(prestEarnings, 10.0.pow(60))).pow(0.20) - 10.0.pow(42).pow(0.20))
                     + max(0.0, (10.0.pow(-6) * prestEarnings).pow(0.21) - 10.0.pow(54).pow(0.21))
             )
+    if(soulEggReward > 1E60) {
+        soulEggReward = eventBoost * (10.0.pow(-6) * prestEarnings).pow(0.21) - 1.56 * 10.0.pow(11)
+    }
+    return soulEggReward
 }
